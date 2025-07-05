bda1445f7c0a05463f639195afd22aa0c697566c
Brendan <brendoniansl@gmail.com>
2025-07-05 16:49:53 -0400

    Update ObtainToken.js

--- redditapp/ObtainToken.js
+++ redditapp/ObtainToken.js
@@ -2,15 +2,15 @@ const client_key = import.meta.env.VITE_REDDIT_KEY;
 const client_secret = import.meta.env.VITE_REDDIT_SECRET;
 
 export async function obtainToken() {
-  const token = await fetch('https://www.reddit.com/api/v1/access_token', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Authorization': 'Basic ' + btoa(client_key + ':' + client_secret)
-    },
-    body: 'grant_type=client_credentials'
-  });
-
-  const data = await token.json();
-  return data.access_token;
+    //Obtains Token
+    const token = await fetch('https://www.reddit.com/api/v1/access_token', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': 'Basic ' + btoa(client_key + ':' + client_secret)
+        },
+        body: 'grant_type=client_credentials'
+    })
+    const data = await token.json();
+    return data.access_token;
 }
