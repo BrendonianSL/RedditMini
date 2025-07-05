4f917251102e2dd4130eb3fbcb5da8d639c3496e
Brendan <brendoniansl@gmail.com>
2025-07-05 16:41:47 -0400

    Updated ENV Files

--- redditapp/ObtainToken.js
+++ redditapp/ObtainToken.js
@@ -1,18 +1,16 @@
-import dotenv from 'dotenv';
-
-const client_key = '4CUDA3w5IkqgpxWK5bnxGQ';
-const client_secret = 'QxF-jhvYD_rYSELjPlFQEqF2Bc3Wkw';
+const client_key = import.meta.env.VITE_REDDIT_KEY;
+const client_secret = import.meta.env.VITE_REDDIT_SECRET;
 
 export async function obtainToken() {
-    //Obtains Token
-    const token = await fetch('https://www.reddit.com/api/v1/access_token', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Authorization': 'Basic ' + btoa(client_key + ':' + client_secret)
-        },
-        body: 'grant_type=client_credentials'
-    })
-    const data = await token.json();
-    return data.access_token;
+  const token = await fetch('https://www.reddit.com/api/v1/access_token', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization': 'Basic ' + btoa(client_key + ':' + client_secret)
+    },
+    body: 'grant_type=client_credentials'
+  });
+
+  const data = await token.json();
+  return data.access_token;
 }
